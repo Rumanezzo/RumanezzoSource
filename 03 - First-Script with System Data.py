e798b99fa9f737ef97adf7fb9b82e6a143e1bca9
Rumanezzo <rumanezzo@gmail.com>
2022-03-17 21:40:32 +0300

    Try - Exception Block Added

--- 03 - First-Script with System Data.py
+++ 03 - First-Script with System Data.py
@@ -1,4 +1,4 @@
-# Не запускать из-под IDE!
+# Не запускать в IDE см. примечание ниже!
 from os import get_terminal_size, getcwd
 from sys import getwindowsversion, version_info, platform
 from math import pi, e, factorial
@@ -8,8 +8,14 @@ print('Версия Операционной системы ->', getwindowsversi
 
 print('Здесь информация о версии Python ->', version_info)
 print('Из модуля os текущий каталог ->', getcwd())
-print('Из модуля os берем размер консоли ->', get_terminal_size())
+try:
+    print('Из модуля os берем размер консоли ->', get_terminal_size())
+except OSError:
+    print('Похоже вы пытаетесь запустить скрипт из-под IDE...')
+
+# Следующая команда "мешает" запустить скрипт в IDE
+
 print('Число π ->', pi)
 print('Число e ->', e)
-print('Вычисляем 100! =', factorial(100))
+print('Вычисляем 1000000! =', factorial(1000000))
 input('Для завершения - нажимайте Enter! -> ')
