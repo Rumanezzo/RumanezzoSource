dfa410c2fd1662d38ff42f90a4f66b87f792456b
Rumanezzo <rumanezzo@gmail.com>
2022-07-09 23:13:28 +0300

    Init_Screen.py - environ added
    Signed-off-by: Rumanezzo <rumanezzo@gmail.com>

--- Init_Screen.py
+++ Init_Screen.py
@@ -1,4 +1,4 @@
-from os import system, get_terminal_size
+from os import system, get_terminal_size, environ
 
 from keyboard import read_event, KEY_DOWN
 from cursor import hide
@@ -24,12 +24,13 @@ def key_pressed(prompt, *key_in):
 
 
 def get_screen():
+
     try:
         x, y = get_terminal_size()
         set_mod(x, y)
 
     except OSError:
-        print('Похоже вы пытаетесь запустить скрипт из-под IDE...')
+        print('Похоже вы пытаетесь запустить скрипт из-под IDE... -> не надо...!')
         x, y = 106, 32
     return x, y
 
@@ -61,33 +62,38 @@ def resize(prompt, key_in, x, y):
 
 def init_screen():
     try:
-        with open('screen.txt', 'r') as f_inst:
-            line = f_inst.readline()
+        with open('screen.txt', 'r') as f_scr:
+            line = f_scr.readline()
 
         line_list = line.split()
         x0, y0 = map(int, line_list)
         set_mod(x0, y0)
 
     except FileNotFoundError:
-        f_inst = open('screen.txt', 'w')
+        f_scr = open('screen.txt', 'w')
         x1, y1 = get_screen()
         x0, y0 = resize('Меняем размер экрана. Просто нажимайте стрелки, а когда надоест - shift', 'shift', x1, y1)
         print(f'Судя по всему Вам понравилась Ширина {x0} и Высота {y0}')
         coord_for_writing = str(x0) + ' ' + str(y0)
-        f_inst.write(coord_for_writing)
+        f_scr.write(coord_for_writing)
 
-    f_inst.close()
+    f_scr.close()
     return x0, y0
 
 
 def main():
     hide()
+
     init_screen()
     key_pressed('Для продолжения нажмите Shift!', 'shift')
 
 
 if __name__ == "__main__":
-    set_mod(106, 20)
+
+    if 'pycharm_hosted' in environ:
+        exit()
+
+    system('mode 106, 32')
     system('title Вы запустили модуль Init_Screen... Зачем вы это сделали?!... Вы об этом пожалеете!')
     print('Вы точно уверены, что вам надо было запускать этот модуль???')
     key_out = key_pressed('Для продолжения нажмите ♦Shift♦, для отказа нажмите ♦Ctrl♦', 'shift', 'ctrl')
