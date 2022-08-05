18b3f105b98be98b1d7771590058b45481e7a9f6
Rumanezzo <rumanezzo@gmail.com>
2022-08-05 17:02:15 +0300

    var shift added
    Signed-off-by: Rumanezzo <rumanezzo@gmail.com>

--- Script for Books.py
+++ Script for Books.py
@@ -1,12 +1,13 @@
 from os import chdir, getcwd, listdir, remove
 # Скрипт удаляет файла из каталога - Внимание!
-chdir('d:/Programs/')
+shift = 3
+chdir('d:/Programs/ScanKromsator/Tempsk/1/')
 print(f'Текущая папочка -> {getcwd()}')
 dirs = listdir()
 print(f'Её содержимое -> {dirs}')
 
 for i in dirs:
     name = int(i[:-4])
-    if name % 4:
+    if (name - shift) % 4:
         print(f'Удаляем {i}')
         remove(i)
