ca4c6248be89cd54cf170e348322dd073de51001
Rumanezzo <rumanezzo@gmail.com>
2023-09-07 01:26:57 +0300

    Small Modifications
    Signed-off-by: Rumanezzo <rumanezzo@gmail.com>

--- Coeffs and Discriminant of Quadratic Equition.py
+++ Coeffs and Discriminant of Quadratic Equition.py
@@ -1,10 +1,14 @@
 smb = ('•', '²')
 
+
 def quadro(a, b, c):
     d = b ** 2 - 4 * a * c
     return f'{a}•x² + {b}•x + {c} = 0', f'D = {b}² - 4•{a}•{c} = {d}'
 
+
 print(quadro(2, 3, 1)[0])
 print(quadro(3, 2, 4)[1])
-
-input('Enter! ↦ ')
+a0, b0, c0 = list(map(int, input('a = ?, b = ?, c = ? ').split()))
+print(quadro(a0, b0, c0)[0])
+print(quadro(a0, b0, c0)[1])
+input('Enter! -> ')
