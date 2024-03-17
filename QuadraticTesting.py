a5829ee3408af6195014c733e801da1e2c5b344b
Rumanezzo <rumanezzo@gmail.com>
2024-03-17 19:58:16 +0300

    Small corrections
    Signed-off-by: Rumanezzo <rumanezzo@gmail.com>

--- QuadraticTesting.py
+++ QuadraticTesting.py
@@ -1,15 +1,20 @@
 from math import sqrt
-while True:
+game = True
+
+while game:
     a, b, c = input('Введите коэффициенты квадратного уравнения через пробел-> ').split()
 
     a, b, c = map(int, (a, b, c))
-    if a == 0: break 
-    d = b**2 - 4 * a * c
+    if a*a + b*b + c*c == 0:
+        game = False
+    if a == 0:
+        break
+    d = b ** 2 - 4 * a * c
     print(f'Вы ввели a={a}, b={b}, c={c} => D={d},', end=' ')
 
     if d > 0:
         print(f'{d}>0 => У нас 2 корня')
-        sqrt_d0 = sqrt(d) 
+        sqrt_d0 = sqrt(d)
         if int(sqrt_d0) == sqrt_d0:
             sqrt_d0 = int(sqrt_d0)
             print(f'{d}={sqrt_d0}² => могу решать!... Но пока не хочу!')
@@ -19,7 +24,7 @@ while True:
         x = -b / (2 * a)
         print(f'а раз так, то у нас 1 корень x={x:.2f}...но это не точно!')
         print(f'На самом деле надо поставить минус перед {b} и поделить на удвоенное {a}')
-        
+
     elif d < 0:
-        print('{d}<0 => У нас (точнее у Вас) *нет* корней')
-print('Раскусил таки, негодник!')
+        print(f'{d}<0 => У нас (точнее у Вас) *нет* корней')
+
